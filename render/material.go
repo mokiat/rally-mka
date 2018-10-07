64d36ea119e03f36b50e9553657359775bba1599
Momchil Atanasov <momchil.dev@gmail.com>
2018-10-07 22:15:10 +0300

    feat(render): improve skycube material

--- render/material.go
+++ render/material.go
@@ -16,6 +16,7 @@ type Material struct {
 	modelMatrixLocation      int32
 	viewMatrixLocation       int32
 	diffuseTextureLocation   int32
+	skycubeTextureLocation   int32
 }
 
 func newMaterial(vertexSrc, fragmentSrc string) *Material {
@@ -53,6 +54,7 @@ func (m *Material) Generate() error {
 	m.modelMatrixLocation = m.program.GetUniformLocation("modelMatrixIn")
 	m.viewMatrixLocation = m.program.GetUniformLocation("viewMatrixIn")
 	m.diffuseTextureLocation = m.program.GetUniformLocation("diffuseTextureIn")
+	m.skycubeTextureLocation = m.program.GetUniformLocation("skycubeTextureIn")
 	return nil
 }
 
--- render/material_skycube.go
+++ render/material_skycube.go
@@ -9,24 +9,25 @@ const skycubeVertexShader string = `#version 410
 uniform mat4 projectionMatrixIn;
 uniform mat4 viewMatrixIn;
 
-in vec3 coordIn;
+layout(location = 0) in vec3 coordIn;
 
 smooth out vec3 texCoordInOut;
 
 void main()
 {
 	// we optimize by using vertex coords as cube texture coords
-	// we need to normalize the coords so that clamping works correctly
-	// additionally, we need to flip the coords. opengl is broken
-	// in that it uses renderman coordinate system for cube maps, 
-	// contrary to the rest of the opengl api. epic fail!
-	texCoordInOut = -normalize(coordIn);
+	// additionally, we need to flip the coords. opengl uses renderman coordinate
+	// system for cube maps, contrary to the rest of the opengl api
+	texCoordInOut = -coordIn;
 
-	// assure that translations are ignored by setting w to 0.0
+	// ensure that translations are ignored by setting w to 0.0
 	vec4 viewPosition = viewMatrixIn * vec4(coordIn, 0.0);
 
 	// restore w to 1.0 so that projection works
-	gl_Position = projectionMatrixIn * vec4(viewPosition.xyz, 1.0);
+	vec4 position = projectionMatrixIn * vec4(viewPosition.xyz, 1.0);
+
+	// set z to w so that it has maximum depth (1.0) after projection division
+	gl_Position = vec4(position.xy, position.w, position.w);
 }
 `
 
