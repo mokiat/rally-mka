4269a61302e7e1153e6efb4bf08f8189e5c183fc
Momchil Atanasov <momchil.dev@gmail.com>
2020-03-07 17:18:13 +0200

    extract app bootstrap code

---
+++ game/app.go
@@ -0,0 +1,85 @@
+package game
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/go-gl/gl/v4.1-core/gl"
+	"github.com/go-gl/glfw/v3.3/glfw"
+)
+
+const (
+	initialWindowWidth  = 1024
+	initialWindowHeight = 576
+	appName             = "Rally MKA"
+)
+
+type Application struct{}
+
+func (a Application) Run() error {
+	if err := glfw.Init(); err != nil {
+		return fmt.Errorf("failed to initialize glfw: %w", err)
+	}
+	defer glfw.Terminate()
+
+	glfw.WindowHint(glfw.ContextVersionMajor, 4)
+	glfw.WindowHint(glfw.ContextVersionMinor, 1)
+	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
+	window, err := glfw.CreateWindow(initialWindowWidth, initialWindowHeight, appName, nil, nil)
+	if err != nil {
+		return fmt.Errorf("failed to create glfw window: %w", err)
+	}
+	defer window.Destroy()
+	window.MakeContextCurrent()
+	window.SetInputMode(glfw.CursorMode, glfw.CursorHidden)
+
+	glfw.SwapInterval(1)
+	if err := gl.Init(); err != nil {
+		return fmt.Errorf("failed to initialize opengl: %w", err)
+	}
+
+	assetsDir := filepath.Join(filepath.Dir(os.Args[0]), "..", "Resources", "assets")
+	if !dirExists(assetsDir) {
+		assetsDir = "assets"
+	}
+
+	controller := NewController(assetsDir)
+	controller.InitScene()
+
+	window.SetFramebufferSizeCallback(func(w *glfw.Window, width int, height int) {
+		controller.ResizeScene(width, height)
+	})
+	fbWidth, fbHeight := window.GetFramebufferSize()
+	controller.ResizeScene(fbWidth, fbHeight)
+
+	for !window.ShouldClose() {
+		isQuit := window.GetKey(glfw.KeyEscape) == glfw.Press
+		if isQuit {
+			break
+		}
+
+		isForward := window.GetKey(glfw.KeyUp) == glfw.Press
+		isBack := window.GetKey(glfw.KeyDown) == glfw.Press
+		isLeft := window.GetKey(glfw.KeyLeft) == glfw.Press
+		isRight := window.GetKey(glfw.KeyRight) == glfw.Press
+		isBrake := window.GetKey(glfw.KeyEnter) == glfw.Press
+		controller.SetFrame(isForward, isBack, isLeft, isRight, isBrake)
+
+		controller.UpdateScene()
+		controller.RenderScene()
+
+		window.SwapBuffers()
+		glfw.PollEvents()
+	}
+	return nil
+}
+
+func dirExists(path string) bool {
+	info, err := os.Stat(path)
+	if err != nil {
+		return false
+	}
+	return info.IsDir()
+}
--- main.go
+++ main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"os"
-	"path/filepath"
+	"log"
 	"runtime"
 
-	"github.com/go-gl/gl/v4.1-core/gl"
-	"github.com/go-gl/glfw/v3.3/glfw"
 	"github.com/mokiat/rally-mka/game"
 )
 
@@ -15,69 +12,12 @@ func init() {
 }
 
 func main() {
-	if err := glfw.Init(); err != nil {
-		panic(err)
-	}
-	defer glfw.Terminate()
+	log.Println("starting application")
 
-	const width = 1024
-	const height = 576
-	glfw.WindowHint(glfw.ContextVersionMajor, 4)
-	glfw.WindowHint(glfw.ContextVersionMinor, 1)
-	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
-	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
-	window, err := glfw.CreateWindow(width, height, "Rally MKA", nil, nil)
-	if err != nil {
-		panic(err)
-	}
-	defer window.Destroy()
-	window.MakeContextCurrent()
-	window.SetInputMode(glfw.CursorMode, glfw.CursorHidden)
-
-	glfw.SwapInterval(1)
-	if err := gl.Init(); err != nil {
-		panic(err)
+	app := game.Application{}
+	if err := app.Run(); err != nil {
+		log.Fatalf("application crashed: %v", err)
 	}
 
-	assetsDir := filepath.Join(filepath.Dir(os.Args[0]), "..", "Resources", "assets")
-	if !dirExists(assetsDir) {
-		assetsDir = "assets"
-	}
-
-	controller := game.NewController(assetsDir)
-	controller.InitScene()
-
-	window.SetFramebufferSizeCallback(func(w *glfw.Window, width int, height int) {
-		controller.ResizeScene(width, height)
-	})
-	fbWidth, fbHeight := window.GetFramebufferSize()
-	controller.ResizeScene(fbWidth, fbHeight)
-
-	for !window.ShouldClose() {
-		isQuit := window.GetKey(glfw.KeyEscape) == glfw.Press
-		if isQuit {
-			break
-		}
-
-		isForward := window.GetKey(glfw.KeyUp) == glfw.Press
-		isBack := window.GetKey(glfw.KeyDown) == glfw.Press
-		isLeft := window.GetKey(glfw.KeyLeft) == glfw.Press
-		isRight := window.GetKey(glfw.KeyRight) == glfw.Press
-		isBrake := window.GetKey(glfw.KeyEnter) == glfw.Press
-		controller.SetFrame(isForward, isBack, isLeft, isRight, isBrake)
-
-		controller.UpdateScene()
-		controller.RenderScene()
-
-		window.SwapBuffers()
-		glfw.PollEvents()
-	}
-}
-
-func dirExists(path string) bool {
-	info, err := os.Stat(path)
-	if err != nil {
-		return false
-	}
-	return info.IsDir()
+	log.Println("application closed")
 }
