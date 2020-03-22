955055073ceba1d61461b071e90acfba3edb848a
Momchil Atanasov <momchil.dev@gmail.com>
2020-03-22 21:10:59 +0200

    add dummy simulation view

---
+++ cmd/rallymka/internal/game/simulation/view.go
@@ -0,0 +1,15 @@
+package simulation
+
+import "github.com/mokiat/rally-mka/internal/engine/graphics"
+
+type View struct{}
+
+func (v *View) Resize(width, height int) {
+}
+
+func (v *View) Update(elapsedSeconds float32) {
+
+}
+
+func (v *View) Render(pipeline *graphics.Pipeline) {
+}
