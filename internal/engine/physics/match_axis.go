631652c79b63840359364c2cc9fbae09792fe7b4
Momchil Atanasov <momchil.dev@gmail.com>
2020-05-01 12:02:12 +0300

    fix match axis constraint

--- internal/engine/physics/match_axis.go
+++ internal/engine/physics/match_axis.go
@@ -28,24 +28,12 @@ func (c MatchAxisConstraint) Calculate() MatchAxisConstraintResult {
 	firstRadiusWS := sprec.QuatVec3Rotation(c.FirstBody.Orientation, c.FirstBodyAxis)
 	secondRadiusWS := sprec.QuatVec3Rotation(c.SecondBody.Orientation, c.SecondBodyAxis)
 	deltaPosition := sprec.Vec3Diff(secondRadiusWS, firstRadiusWS)
-	normal := sprec.BasisZVec3()
-	if deltaPosition.SqrLength() > 0.000001 {
-		normal = sprec.UnitVec3(deltaPosition)
-	}
 	return MatchAxisConstraintResult{
 		Jacobian: DoubleBodyJacobian{
-			SlopeVelocityFirst: sprec.ZeroVec3(),
-			SlopeAngularVelocityFirst: sprec.NewVec3(
-				-(normal.Z*firstRadiusWS.Y - normal.Y*firstRadiusWS.Z),
-				-(normal.X*firstRadiusWS.Z - normal.Z*firstRadiusWS.X),
-				-(normal.Y*firstRadiusWS.X - normal.X*firstRadiusWS.Y),
-			),
-			SlopeVelocitySecond: sprec.ZeroVec3(),
-			SlopeAngularVelocitySecond: sprec.NewVec3(
-				(normal.Z*secondRadiusWS.Y - normal.Y*secondRadiusWS.Z),
-				(normal.X*secondRadiusWS.Z - normal.Z*secondRadiusWS.X),
-				(normal.Y*secondRadiusWS.X - normal.X*secondRadiusWS.Y),
-			),
+			SlopeVelocityFirst:         sprec.ZeroVec3(),
+			SlopeAngularVelocityFirst:  sprec.InverseVec3(sprec.Vec3Cross(firstRadiusWS, secondRadiusWS)),
+			SlopeVelocitySecond:        sprec.ZeroVec3(),
+			SlopeAngularVelocitySecond: sprec.Vec3Cross(firstRadiusWS, secondRadiusWS),
 		},
 		Drift: deltaPosition.Length(),
 	}
