97b9763782829932200c93f06c5526f76104d1c5
Momchil Atanasov <momchil.dev@gmail.com>
2021-02-01 21:54:48 +0200

    bump lacking

--- cmd/rallypack/main.go
+++ cmd/rallypack/main.go
@@ -1,149 +1,133 @@
 package main
 
 import (
+	"log"
+
 	"github.com/mokiat/lacking/data/asset"
 	"github.com/mokiat/lacking/data/pack"
 )
 
+const workerCount = 4
+
 func main() {
 	packer := pack.NewPacker()
 
-	// Programs
-	packer.Pipeline(func(p *pack.Pipeline) {
-		p.SaveProgramAsset("assets/programs/forward-albedo.dat",
-			p.BuildProgram(
-				pack.WithVertexShader(p.OpenShaderResource("resources/shaders/forward-albedo.vert")),
-				pack.WithFragmentShader(p.OpenShaderResource("resources/shaders/forward-albedo.frag")),
-			),
-		)
+	packer.Schedule(pack.SaveProgramAsset("assets/programs/forward-albedo.dat",
+		pack.BuildProgram().
+			WithVertexShader(pack.OpenShaderResource("resources/shaders/forward-albedo.vert")).
+			WithFragmentShader(pack.OpenShaderResource("resources/shaders/forward-albedo.frag")),
+	))
 
-		p.SaveProgramAsset("assets/programs/forward-debug.dat",
-			p.BuildProgram(
-				pack.WithVertexShader(p.OpenShaderResource("resources/shaders/forward-debug.vert")),
-				pack.WithFragmentShader(p.OpenShaderResource("resources/shaders/forward-debug.frag")),
-			),
-		)
+	packer.Schedule(pack.SaveProgramAsset("assets/programs/forward-debug.dat",
+		pack.BuildProgram().
+			WithVertexShader(pack.OpenShaderResource("resources/shaders/forward-debug.vert")).
+			WithFragmentShader(pack.OpenShaderResource("resources/shaders/forward-debug.frag")),
+	))
 
-		p.SaveProgramAsset("assets/programs/geometry-skybox.dat",
-			p.BuildProgram(
-				pack.WithVertexShader(p.OpenShaderResource("resources/shaders/geometry-skybox.vert")),
-				pack.WithFragmentShader(p.OpenShaderResource("resources/shaders/geometry-skybox.frag")),
-			),
-		)
+	packer.Schedule(pack.SaveProgramAsset("assets/programs/geometry-skybox.dat",
+		pack.BuildProgram().
+			WithVertexShader(pack.OpenShaderResource("resources/shaders/geometry-skybox.vert")).
+			WithFragmentShader(pack.OpenShaderResource("resources/shaders/geometry-skybox.frag")),
+	))
 
-		p.SaveProgramAsset("assets/programs/lighting-pbr.dat",
-			p.BuildProgram(
-				pack.WithVertexShader(p.OpenShaderResource("resources/shaders/lighting-pbr.vert")),
-				pack.WithFragmentShader(p.OpenShaderResource("resources/shaders/lighting-pbr.frag")),
-			),
-		)
-	})
+	packer.Schedule(pack.SaveProgramAsset("assets/programs/lighting-pbr.dat",
+		pack.BuildProgram().
+			WithVertexShader(pack.OpenShaderResource("resources/shaders/lighting-pbr.vert")).
+			WithFragmentShader(pack.OpenShaderResource("resources/shaders/lighting-pbr.frag")),
+	))
 
-	// TwoD Textures
-	packer.Pipeline(func(p *pack.Pipeline) {
-		p.SaveTwoDTextureAsset("assets/textures/twod/loading.dat",
-			p.OpenImageResource("resources/images/loading.png"),
-		)
+	packer.Schedule(pack.SaveTwoDTextureAsset("assets/textures/twod/loading.dat",
+		pack.OpenImageResource("resources/images/loading.png"),
+	))
 
-		p.SaveTwoDTextureAsset("assets/textures/twod/concrete.dat",
-			p.OpenImageResource("resources/images/concrete.png"),
-		)
+	packer.Schedule(pack.SaveTwoDTextureAsset("assets/textures/twod/concrete.dat",
+		pack.OpenImageResource("resources/images/concrete.png"),
+	))
 
-		p.SaveTwoDTextureAsset("assets/textures/twod/road.dat",
-			p.OpenImageResource("resources/images/road.png"),
-		)
+	packer.Schedule(pack.SaveTwoDTextureAsset("assets/textures/twod/road.dat",
+		pack.OpenImageResource("resources/images/road.png"),
+	))
 
-		p.SaveTwoDTextureAsset("assets/textures/twod/barrier.dat",
-			p.OpenImageResource("resources/images/barrier.png"),
-		)
+	packer.Schedule(pack.SaveTwoDTextureAsset("assets/textures/twod/barrier.dat",
+		pack.OpenImageResource("resources/images/barrier.png"),
+	))
 
-		p.SaveTwoDTextureAsset("assets/textures/twod/grass.dat",
-			p.OpenImageResource("resources/images/grass.png"),
-		)
+	packer.Schedule(pack.SaveTwoDTextureAsset("assets/textures/twod/grass.dat",
+		pack.OpenImageResource("resources/images/grass.png"),
+	))
 
-		p.SaveTwoDTextureAsset("assets/textures/twod/gravel.dat",
-			p.OpenImageResource("resources/images/gravel.png"),
-		)
+	packer.Schedule(pack.SaveTwoDTextureAsset("assets/textures/twod/gravel.dat",
+		pack.OpenImageResource("resources/images/gravel.png"),
+	))
 
-		p.SaveTwoDTextureAsset("assets/textures/twod/rusty_metal_02_diff_512.dat",
-			p.OpenImageResource("resources/images/rusty_metal_02_diff_512.png"),
-		)
+	packer.Schedule(pack.SaveTwoDTextureAsset("assets/textures/twod/rusty_metal_02_diff_512.dat",
+		pack.OpenImageResource("resources/images/rusty_metal_02_diff_512.png"),
+	))
 
-		p.SaveTwoDTextureAsset("assets/textures/twod/body.dat",
-			p.OpenImageResource("resources/images/body.png"),
-		)
+	packer.Schedule(pack.SaveTwoDTextureAsset("assets/textures/twod/body.dat",
+		pack.OpenImageResource("resources/images/body.png"),
+	))
 
-		p.SaveTwoDTextureAsset("assets/textures/twod/wheel.dat",
-			p.OpenImageResource("resources/images/wheel.png"),
-		)
+	packer.Schedule(pack.SaveTwoDTextureAsset("assets/textures/twod/wheel.dat",
+		pack.OpenImageResource("resources/images/wheel.png"),
+	))
 
-		p.SaveTwoDTextureAsset("assets/textures/twod/leafy_tree.dat",
-			p.OpenImageResource("resources/images/leafy_tree.png"),
-		)
-	})
+	packer.Schedule(pack.SaveTwoDTextureAsset("assets/textures/twod/leafy_tree.dat",
+		pack.OpenImageResource("resources/images/leafy_tree.png"),
+	))
 
-	// Cube Textures
-	packer.Pipeline(func(p *pack.Pipeline) {
-		srcEquirectangularImage := p.OpenImageResource("resources/images/syferfontein.hdr")
-		skyboxCubeImage := p.BuildCubeImage(
-			pack.WithFrontImage(p.BuildCubeSideFromEquirectangular(pack.CubeSideFront, srcEquirectangularImage)),
-			pack.WithRearImage(p.BuildCubeSideFromEquirectangular(pack.CubeSideRear, srcEquirectangularImage)),
-			pack.WithLeftImage(p.BuildCubeSideFromEquirectangular(pack.CubeSideLeft, srcEquirectangularImage)),
-			pack.WithRightImage(p.BuildCubeSideFromEquirectangular(pack.CubeSideRight, srcEquirectangularImage)),
-			pack.WithTopImage(p.BuildCubeSideFromEquirectangular(pack.CubeSideTop, srcEquirectangularImage)),
-			pack.WithBottomImage(p.BuildCubeSideFromEquirectangular(pack.CubeSideBottom, srcEquirectangularImage)),
-		)
+	srcEquirectangularImage := pack.OpenImageResource("resources/images/syferfontein.hdr")
+	skyboxCubeImage := pack.BuildCubeImage().
+		WithFrontImage(pack.BuildCubeSideFromEquirectangular(pack.CubeSideFront, srcEquirectangularImage)).
+		WithRearImage(pack.BuildCubeSideFromEquirectangular(pack.CubeSideRear, srcEquirectangularImage)).
+		WithLeftImage(pack.BuildCubeSideFromEquirectangular(pack.CubeSideLeft, srcEquirectangularImage)).
+		WithRightImage(pack.BuildCubeSideFromEquirectangular(pack.CubeSideRight, srcEquirectangularImage)).
+		WithTopImage(pack.BuildCubeSideFromEquirectangular(pack.CubeSideTop, srcEquirectangularImage)).
+		WithBottomImage(pack.BuildCubeSideFromEquirectangular(pack.CubeSideBottom, srcEquirectangularImage))
+	packer.Schedule(pack.SaveCubeTextureAsset("assets/textures/cube/syferfontein.dat", skyboxCubeImage).
+		WithFormat(asset.DataFormatRGBA32F),
+	)
 
-		p.SaveCubeTextureAsset("assets/textures/cube/syferfontein.dat", skyboxCubeImage,
-			pack.WithFormat(asset.DataFormatRGBA32F),
-		)
+	reflectionCubeImage := pack.ScaleCubeImage(skyboxCubeImage, 128)
+	packer.Schedule(pack.SaveCubeTextureAsset("assets/textures/cube/syferfontein_reflection.dat", reflectionCubeImage).
+		WithFormat(asset.DataFormatRGBA32F),
+	)
 
-		reflectionCubeImage := p.ScaleCubeImage(skyboxCubeImage, 128)
-		p.SaveCubeTextureAsset("assets/textures/cube/syferfontein_reflection.dat", reflectionCubeImage,
-			pack.WithFormat(asset.DataFormatRGBA32F),
-		)
+	refractionCubeImage := pack.BuildIrradianceCubeImage(reflectionCubeImage).
+		WithSampleCount(50)
+	packer.Schedule(pack.SaveCubeTextureAsset("assets/textures/cube/syferfontein_refraction.dat", refractionCubeImage).
+		WithFormat(asset.DataFormatRGBA32F),
+	)
 
-		refractionCubeImage := p.BuildIrradianceCubeImage(reflectionCubeImage,
-			pack.WithSampleCount(50),
-		)
-		p.SaveCubeTextureAsset("assets/textures/cube/syferfontein_refraction.dat", refractionCubeImage,
-			pack.WithFormat(asset.DataFormatRGBA32F),
-		)
-	})
+	packer.Schedule(pack.SaveModelAsset("assets/models/quad.dat",
+		pack.OpenGLTFResource("resources/models/quad.gltf"),
+	))
 
-	// Models
-	packer.Pipeline(func(p *pack.Pipeline) {
-		p.SaveModelAsset("assets/models/quad.dat",
-			p.OpenGLTFResource("resources/models/quad.gltf"),
-		)
+	packer.Schedule(pack.SaveModelAsset("assets/models/street_lamp.dat",
+		pack.OpenGLTFResource("resources/models/street_lamp.gltf"),
+	))
 
-		p.SaveModelAsset("assets/models/street_lamp.dat",
-			p.OpenGLTFResource("resources/models/street_lamp.gltf"),
-		)
+	packer.Schedule(pack.SaveModelAsset("assets/models/suv.dat",
+		pack.OpenGLTFResource("resources/models/suv.gltf"),
+	))
 
-		p.SaveModelAsset("assets/models/suv.dat",
-			p.OpenGLTFResource("resources/models/suv.gltf"),
-		)
+	packer.Schedule(pack.SaveModelAsset("assets/models/leafy_tree.dat",
+		pack.OpenGLTFResource("resources/models/leafy_tree.gltf"),
+	))
 
-		p.SaveModelAsset("assets/models/leafy_tree.dat",
-			p.OpenGLTFResource("resources/models/leafy_tree.gltf"),
-		)
-	})
+	packer.Schedule(pack.SaveLevelAsset("assets/levels/forest.dat",
+		pack.OpenLevelResource("resources/levels/forest.json"),
+	))
 
-	// Levels
-	packer.Pipeline(func(p *pack.Pipeline) {
-		p.SaveLevelAsset("assets/levels/forest.dat",
-			p.OpenLevelResource("resources/levels/forest.json"),
-		)
+	packer.Schedule(pack.SaveLevelAsset("assets/levels/highway.dat",
+		pack.OpenLevelResource("resources/levels/highway.json"),
+	))
 
-		p.SaveLevelAsset("assets/levels/highway.dat",
-			p.OpenLevelResource("resources/levels/highway.json"),
-		)
+	packer.Schedule(pack.SaveLevelAsset("assets/levels/playground.dat",
+		pack.OpenLevelResource("resources/levels/playground.json"),
+	))
 
-		p.SaveLevelAsset("assets/levels/playground.dat",
-			p.OpenLevelResource("resources/levels/playground.json"),
-		)
-	})
-
-	packer.RunParallel()
+	if err := packer.Run(workerCount); err != nil {
+		log.Fatalf("packer error: %s", err)
+	}
 }
