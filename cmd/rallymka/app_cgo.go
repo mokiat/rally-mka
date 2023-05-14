54cf75b11e37524394ce3828927d944b8d108b84
Momchil Atanasov <momchil.dev@gmail.com>
2023-05-14 15:58:34 +0300

    Embed all resources

--- cmd/rallymka/app_cgo.go
+++ cmd/rallymka/app_cgo.go
@@ -16,6 +16,7 @@ import (
 	"github.com/mokiat/lacking/ui/mat"
 	"github.com/mokiat/lacking/util/resource"
 	gameui "github.com/mokiat/rally-mka/internal/ui"
+	"github.com/mokiat/rally-mka/resources"
 )
 
 func runApplication() error {
@@ -23,11 +24,11 @@ func runApplication() error {
 	if err != nil {
 		return fmt.Errorf("failed to initialize registry: %w", err)
 	}
+	locator := mat.WrappedResourceLocator(resource.NewFSLocator(resources.UI))
 
 	renderAPI := glrender.NewAPI()
 	gameController := game.NewController(registry, renderAPI, glgame.NewShaderCollection())
-	resourceLocator := mat.WrappedResourceLocator(resource.NewFileLocator("./resources"))
-	uiCfg := ui.NewConfig(resourceLocator, renderAPI, glui.NewShaderCollection())
+	uiCfg := ui.NewConfig(locator, renderAPI, glui.NewShaderCollection())
 	uiController := ui.NewController(uiCfg, func(w *ui.Window) {
 		gameui.BootstrapApplication(w, gameController)
 	})
@@ -37,7 +38,8 @@ func runApplication() error {
 	cfg.SetMaximized(false)
 	cfg.SetMinSize(1024, 576)
 	cfg.SetVSync(true)
-	cfg.SetIcon("resources/ui/images/icon.png")
+	cfg.SetIcon("ui/images/icon.png")
 	cfg.SetMaximized(true)
+	cfg.SetLocator(locator)
 	return glapp.Run(cfg, app.NewLayeredController(gameController, uiController))
 }
